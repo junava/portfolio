0df4ed6e13b6391d7d8c0a1a0ee4d5b847427e0f
junava <navajoachim@gmail.com>
2026-02-06 22:12:15 +0800

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+function locationURL() {
+    location.href = "/html/page2.html";
+}   
+function locationURL() {
+    location.href = "/html/calc.html";
+}  
+function showAlert() {
+    alert("This is a simple calculator!");
+let num1 = alert.prompt("Enter the first number:");
+let num2 = alert.prompt("Enter the second number:");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+alert("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+if (isNaN(num1) || isNaN(num2)) {
+    console.log("Invalid input. Please enter numeric values.");
+} else {
+    const sum = num1 + num2;
+    console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
